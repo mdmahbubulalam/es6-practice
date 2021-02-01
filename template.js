134345f64743530fd298d5eea22075db78bef43c
mdmahbubulalam <alam.diu13@gmail.com>
2021-02-01 20:15:21 +0600

    Template string, multiple line string

---
+++ template.js
@@ -0,0 +1,18 @@
+const firstName = "Justin";
+const lastName = "Timberlake";
+const fullName = firstName+" "+lastName + " is a good boy";
+const fullName2 =`${fullName} is a good boy`;
+const fullName3 =`${firstName} ${lastName}...Are you there? Your total bill ${10+20+30} Taka.`;
+console.log(fullName3);
+
+const multiLine = "And all the roads we have to walk are winding \n"
++"And all the lights that lead us there are blinding \n"
++"There are many things that I \n"
++"Would like to say to you but I don't know how \n";
+
+const multiLine2 =`And all the roads we have to walk are winding
+And all the lights that lead us there are blinding
+There are many things that I
+Would like to say to you but I don't know how`
+
+console.log(multiLine2);
